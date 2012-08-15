ff6da0597f1f9c806a4d395562fd717b80f6289f
James Tomasino <git@jamestomasino.com>
2012-08-15 14:12:33 -0400

    added getNextCuePoint and getPreviousCuePoint

--- org/incrediberry/video/VideoCuePoints.class.js
+++ org/incrediberry/video/VideoCuePoints.class.js
@@ -47,22 +47,76 @@
 			this.video.addEventListener ( "seeked", this._onSeekedDelegate );
 		},
 
+		getNextCuePoint: function ( ) {
+
+			// If no video loaded or not started, return null
+			if ( this.time == -1 || this.video === null ) return null;
+
+			var cuePoint = this.firstCuePoint;
+
+			while (cuePoint) {
+				if (this.time >= cuePoint.time) {
+					// Time is ahead of cuepoint. Skip it
+				} else {
+					// Cuepoint is ahead of time. That's it.
+					var o = { name: cuePoint.name, time: cuePoint.time, type: cuePoint.type };
+					return o;
+				}
+				cuePoint = cuePoint.next;
+			}
+
+			// if nothing found, return null
+			return null;
+
+		},
+
+		getPrevCuePoint: function ( ) {
+			// This function needs a buffer after a cuepoint where it can jump back earlier even if it is after
+			var bufferPrevTime = 1;
+
+			// If no video loaded or not started, return null
+			if ( this.time == -1 || this.video === null ) return null;
+
+			var cuePoint = this.firstCuePoint;
+			var lastCuePoint = null;
+
+			while (cuePoint) {
+				if (this.time - bufferPrevTime >= cuePoint.time) {
+					// Time is ahead of cuepoint. Skip it
+				} else {
+					// Cuepoint is ahead of time. previous cuepoint is one back (will this work?)
+					if (lastCuePoint) {
+						return { name: lastCuePoint.name, time: lastCuePoint.time, type: lastCuePoint.type };
+					} else {
+						return null;
+					}
+				}
+
+				lastCuePoint = cuePoint;
+				cuePoint = cuePoint.next;
+			}
+
+			// if nothing found, return null
+			return lastCuePoint;
+
+		},
+
 		addCuePoint: function ( time, label ) {
 			var prevCuePoint = this.firstCuePoint;
 
-			if ( prevCuePoint != null && prevCuePoint.time > time ) {
+			if ( prevCuePoint !== null && prevCuePoint.time > time ) {
 				prevCuePoint = null;
 			} else {
-				while (prevCuePoint != null && prevCuePoint.time <= time && prevCuePoint.next != null && prevCuePoint.next.time <= time) {
+				while (prevCuePoint !== null && prevCuePoint.time <= time && prevCuePoint.next !== null && prevCuePoint.next.time <= time) {
 					prevCuePoint = prevCuePoint.next;
 				}
 			}
 
 			var cuePoint = new this.CuePoint ( time, label, VideoCuePoints.CUE_POINT_TYPE, prevCuePoint );
 
-			if ( prevCuePoint == null )
+			if ( prevCuePoint === null )
 			{
-				if ( this.firstCuePoint != null )
+				if ( this.firstCuePoint !== null )
 				{
 					this.firstCuePoint.prev = cuePoint;
 					cuePoint.next = this.firstCuePoint;
@@ -75,13 +129,13 @@
 		removeCuePoint: function ( timeNameOrCuePoint ) {
 			var cuePoint = this.firstCuePoint;
 
-			while (cuePoint != null) {
-				if (cuePoint == timeNameOrCuePoint || cuePoint.time == timeNameOrCuePoint || cuePoint.name == timeNameOrCuePoint) {
-					if (cuePoint.next != null) {
+			while (cuePoint !== null) {
+				if (cuePoint === timeNameOrCuePoint || cuePoint.time == timeNameOrCuePoint || cuePoint.name == timeNameOrCuePoint) {
+					if (cuePoint.next !== null) {
 						cuePoint.next.prev = cuePoint.prev;
 					}
 
-					if (cuePoint.prev != null) {
+					if (cuePoint.prev !== null) {
 						cuePoint.prev.next = cuePoint.next;
 					} else if (cuePoint == this.firstCuePoint) {
 						this.firstCuePoint = cuePoint.next;
@@ -99,7 +153,7 @@
 		},
 
 		_onTimeUpdate: function ( e ) {
-			if ( this.firstCuePoint != null ) {
+			if ( this.firstCuePoint !== null ) {
 				if ( this.time != this.video.currentTime ) {
 					if (!this.isSeeking) {
 						var nextCuePoint = null;
@@ -109,7 +163,7 @@
 							nextCuePoint = cuePoint.next;
 
 							if ( this.time < cuePoint.time && cuePoint.time <= this.video.currentTime ) {
-								var o = { name: cuePoint.name, time: cuePoint.time, type: cuePoint.type }
+								var o = { name: cuePoint.name, time: cuePoint.time, type: cuePoint.type };
 								this.dispatchEvent ( VideoCuePoints.CUE_POINT_EVENT, o );
 							}
 
